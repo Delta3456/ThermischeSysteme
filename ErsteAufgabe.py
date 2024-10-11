04c6ae2fea3cab2467e5789220e99e09baa69a54
Delta3456 <j.focken@gmx.de>
2024-10-11 18:03:37 +0200

    Erste Funktion mit Ausgabe steht

--- ErsteAufgabe.py
+++ ErsteAufgabe.py
@@ -10,6 +10,7 @@ Aufgabe 1 - Wärmeübertragung in einem Rohr
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # Funktion: Nusselt nach Aufgabe 1a
 def nusselt_a(re, pr, n=1):
     """
@@ -21,33 +22,29 @@ def nusselt_a(re, pr, n=1):
     :param n: Exponent
     :return nu_a: Nusselt-Zahl
     """
-    """
-    # Alter weg 
-    if re < 10000:
-        return np.nan     # Ungültige Reynolds-Zahl
-    if pr <= 0.6 or pr >= 160:
-        return np.nan     # Ungültige Prandtl-Zahl
-
-    return 0.023 * re ** (4 / 5) * pr ** n  # Berechnung Nusselt-Zahl nach a
-    """
     nu_a = np.where(
-        (re >= 10000) & (pr > 0.6) & (pr < 160),  # Bedingung für gültige Reynolds-Zahl und Prandtl-Zahl
-        0.023 * re ** (4 / 5) * pr ** n,          # Berechnung von Nusselt-Zahl nach a falls Bedingungen erfüllt
-        np.nan                                    # NaN falls Bedingungen nicht erfüllt
+        (re >= 10000) & (pr >= 0.6) & (pr <= 160),  # Bedingung für gültige Reynolds-Zahl und Prandtl-Zahl
+        0.023 * re ** (4 / 5) * pr ** n,  # Berechnung von Nusselt-Zahl nach a falls Bedingungen erfüllt
+        np.nan  # NaN, falls Bedingungen nicht erfüllt
     )
     return nu_a
 
+
 # Variablen 1a
-re_array = np.linspace(10000,2e5)  # Reynolds-Zahl Vektor
-Prandtl_values = np.array([0.707, 5.83, 151]) # Prandtl-Zahl für Luft, Wasser, Ethylenglykol bei 300K
+re_array = np.linspace(10000, 2e5)  # Reynolds-Zahl Vektor
+Prandtl_values = np.array([0.707, 5.83, 151])  # Prandtl-Zahl für Luft, Wasser, Ethylenglykol bei 300K
+stoffe = ['Luft', 'Wasser', 'Ethylenglykol']  # Die dazugehörigen Stoffe
 
-Nu_a = nusselt_a(re_array,0.707)
-
-# Ausgabe Ploten
+# Ausgabe plotten
 plt.style.use('ggplot')
 plt.figure()
-plt.plot(re_array,Nu_a)
+
+# Berechnung der Nusselt-Zahl für jeden Stoff
+for pr_value, stoff in zip(Prandtl_values, stoffe):
+    Nu_a = nusselt_a(re_array, pr_value)
+    plt.plot(re_array, Nu_a, label=f'{stoff} (Pr = {pr_value})')
+
 plt.xlabel('Reynolds-Zahl')
 plt.ylabel('Nusselt-Zahl')
-#plt.legend()
+plt.legend(title='Stoffe', loc='upper left')
 plt.show()
