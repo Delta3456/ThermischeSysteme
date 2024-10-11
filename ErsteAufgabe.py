7dab9e4e75fbf8e899dd320669453673a22397aa
Delta3456 <j.focken@gmx.de>
2024-10-11 18:21:38 +0200

    zweite Nusseltzahl

--- ErsteAufgabe.py
+++ ErsteAufgabe.py
@@ -11,10 +11,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Funktion: Nusselt nach Aufgabe 1a
+# Funktionen ------------------------------------------
 def nusselt_a(re, pr, n=1):
     """
-    Funktion berechnet die Nusselt-Zahl nach Aufgabe 1a
+    Funktion berechnet die Nusselt-Zahl nach a
     Bedingungen: turbulent, 0.6≤pr≤160, re≥10000
     -----
     :param re: Reynoldszahl
@@ -30,8 +30,28 @@ def nusselt_a(re, pr, n=1):
     return nu_a
 
 
+def nusselt_b(re, pr, n=1):
+    """
+    Funktion berechnet die Nusselt-Zahl nach b
+    Bedingungen: turbulent, 0.5≤pr≤2000, 3000≤re≤5e6
+    -----
+    :param re: Reynoldszahl
+    :param pr: Prandtl-Zahl
+    :param n: Exponent
+    :return nu_a: Nusselt-Zahl
+    """
+    f=(0.79 * np.log(re) - 1.64)**(-2)
+
+    nu_b = np.where(
+        (re >= 3000) & (re <= 5e6) & (pr >= 0.5) & (pr <= 2000),  # Bedingung für gültige Reynolds-Zahl und Prandtl-Zahl
+        ((f/8)*(re-1000.)*pr)/(1+12.7*(((f/8)**(1/2))*(pr**(2/3)-1))),  # Berechnung von Nusselt-Zahl nach b falls Bedingungen erfüllt
+        np.nan  # NaN, falls Bedingungen nicht erfüllt
+    )
+    return nu_b
+
+
 # Variablen 1a
-re_array = np.linspace(10000, 2e5)  # Reynolds-Zahl Vektor
+re_array = np.linspace(3000, 5e56)  # Reynolds-Zahl Vektor
 Prandtl_values = np.array([0.707, 5.83, 151])  # Prandtl-Zahl für Luft, Wasser, Ethylenglykol bei 300K
 stoffe = ['Luft', 'Wasser', 'Ethylenglykol']  # Die dazugehörigen Stoffe
 
@@ -42,6 +62,7 @@ plt.figure()
 # Berechnung der Nusselt-Zahl für jeden Stoff
 for pr_value, stoff in zip(Prandtl_values, stoffe):
     Nu_a = nusselt_a(re_array, pr_value)
+    Nu_b = nusselt_b(re_array, pr_value)
     plt.plot(re_array, Nu_a, label=f'{stoff} (Pr = {pr_value})')
 
 plt.xlabel('Reynolds-Zahl')
