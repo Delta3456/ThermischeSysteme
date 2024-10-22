92a9800a2c9c2ae41702496f9bb096b59af89295
Delta3456 <j.focken@gmx.de>
2024-10-22 18:43:48 +0200

    Erste Funktion für Teilaufgabe 1 funktioniert

--- DritteAufgabe.py
+++ DritteAufgabe.py
@@ -8,23 +8,49 @@ Teilaufgabe 2 - Verdampfer
 """
 import numpy as np
 from scipy.optimize import root
+import matplotlib.pyplot as plt
 
 
-# Funktionen
-def thermoelement(T, epsilon):
+# Funktionen --------------------------------------------------------------------
+def thermoelement(t_te, epsilon):
     """
-    Funktion zur Energiebilanz des Thermoelements
-    Energiebilanz: 0 = (A * alpha * (T_f - T)) - (A * epsilon * sigma * (T_u**4 - T_u**4))
+    Die Energiebilanz lautet:
+    alpha * (T_f - t_e) = epsilon * sigma * (t_e^4 - T_u^4)
+    ---
+    :param t_te: Temperatur des Thermoelements (K)
+    :param epsilon: Emissionsgrad (-)
     """
-    thermoelement_wert = (alpha * (T_f - T)) - (epsilon * sigma * (T**4 - T_u**4))
+    thermoelement_wert = (alpha * (T_f - t_te)) - (epsilon * sigma * (t_te ** 4 - T_u ** 4))
     return thermoelement_wert
 
+
+def berechne_t_te(epsilon):
+    # Funktion zur Berechnung der Thermoelement-Temperatur für einen gegebenen Emissionsgrad
+    loesung = root(thermoelement, t_te_schaetz, args=(epsilon,))
+    if loesung.success:
+        return loesung.x[0]
+    else:
+        print(f"Fehler!")
+        return None
+
+
+# Teilaufgabe 1 --------------------------------------------------------------------
 # Parameter
 T_f = 1300  # Flammentemperatur in K
 T_u = 300  # Umgebungstemperatur in K
 alpha = 100  # Wärmeübergangskoeffizient W/(m^2*K)
 sigma = 5.67e-8  # Stefan-Boltzmann-Konstante in W/(m^2*K^4)
-epsilon = 0.5
-# Teilaufgabe 1 ----------------------------------------------
-solution = root(thermoelement, 600, args=(epsilon,))
-print(solution)
+t_te_schaetz = np.array([1000])  # Anfangsschätzwert für die Thermoelementtemperatur
+epsilon_werte = np.linspace(0, 1, 21)  # Emissionsgrade von 0 bis 1 in Schritten von 0.05
+
+# Berechnen von Temperaturwerte für verschiedene Emissionsgrade
+t_te_werte = [berechne_t_te(epsilon) for epsilon in epsilon_werte]
+
+# Plot erstellen
+plt.figure(figsize=(8, 6))
+plt.plot(epsilon_werte, t_te_werte)
+plt.title('Temperatur des Thermoelements in Abhängigkeit vom Emissionsgrad')
+plt.xlabel('Emissionsgrad (-)')
+plt.ylabel('Temperatur des Thermoelements (K)')
+plt.grid(True)
+plt.show()
