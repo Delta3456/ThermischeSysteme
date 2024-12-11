1173e233ecad3099d0ddf38e480f06eede62eef9
Delta3456 <j.focken@gmx.de>
2024-12-11 12:32:34 +0100

    Variable Endzeit eingefügt und Ausgabe verbessert

--- 10_Aufgabe.py
+++ 10_Aufgabe.py
@@ -13,6 +13,7 @@ T_u = 20.0       # Umgebungstemperatur
 eps = 0.5   # Absorptionsgrad
 h = 5.0            # Konvektiver Wärmeübergang W/(m²·K)
 sigma = 5.67e-8     # Stefan-Boltzmann-Konstante W/(m²·K^4)
+endtime = 300.0
 dt = 0.1 # Zeitschritt für die Brechnung
 
 # Stoffdaten Stahl
@@ -27,7 +28,7 @@ b = 0.02           # Breite [m]
 laser_width = 0.005  # Breite der Laserzone [m]
 
 # Gitter
-dxy = 1e-4
+dxy = 1e-3
 nx = int(L / dxy) # Zellenanzahl in x-Richtung
 mesh = fp.Grid1D(dx=d, nx=nx)
 x = mesh.x.value
@@ -43,7 +44,7 @@ def laser(P_l, x):
     quel_l[bereich] = (P_l * eps) / (d * b * laser_width)
     return quel_l
 
-def berechnung(P_l, Konvektion=False, endtime=60, dt=dt):
+def berechnung(P_l, Konvektion=False, endtime=endtime, dt=dt):
     # Zellenvariablen
     temp = fp.CellVariable(name="Temperature", mesh=mesh, value=T_u)
     quel_l = laser(P_l, x)
@@ -89,9 +90,15 @@ for sz in szenarien:
     print(f"\nErgebnisse für {sz['label']}:")
     if T_1_grenz is not None:
         print(f"Zeit bis 80°C: {T_1_grenz:.2f} s")
+    else:
+        print(f"Zeit bis 80°C: Temperatur nicht erreicht, innerhalb von {endtime} s")
     if T_2_grenz is not None:
         print(f"Zeit bis 180°C: {T_2_grenz:.2f} s")
-    print(f"Max. Temperatur nach 60 s: {max_temps[-1]:.2f} °C")
+    else:
+        print(f"Zeit bis 180°C: Temperatur nicht erreicht, innerhalb von {endtime} s")
+    if 60 in times:
+        idx = np.where(times == 60)[0][0]  # Ersten Treffer nehmen
+        print(f"Max. Temperatur nach 60 s: {max_temps[idx]:.2f} °C")
     print("--------------------------------------------------------")
 
 plt.xlabel("Zeit [s]")
